21a9c458fbd4772714bd89eab3aa190f28a07a84
cjsj166 <94033188+cjsj166@users.noreply.github.com>
2022-01-03 22:44:29 +0900

    timer implemented

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,25 +1,32 @@
-import { Icon } from "./Icon.types"
-import { ToDoList } from "./ToDoList"
-import { Canvas } from "./Canvas"
-import { Timer } from "./Timer"
+import { Icon } from "./Icon.types";
+import { ToDoList } from "./ToDoList";
+import { Canvas } from "./Canvas";
+import { Timer } from "./Timer";
 
 type DisplayProps = {
-    selectedIcon : Icon | null
-}
+  selectedIcon: Icon | null;
+};
 
-export const Display = ({selectedIcon} : DisplayProps) => {
-    
-    if(selectedIcon === Icon.ToDoList){
-        return <div><ToDoList/></div>
-    }
-    else if(selectedIcon === Icon.Timer) {
-        return <div><Timer/></div>
-    }
-    else if(selectedIcon === Icon.Canvas) {
-        return <div><Canvas/></div>
-    }
-    else{
-        return <div></div>
-    }
-    
-}
+export const Display = ({ selectedIcon }: DisplayProps) => {
+  if (selectedIcon === Icon.ToDoList) {
+    return (
+      <div>
+        <ToDoList />
+      </div>
+    );
+  } else if (selectedIcon === Icon.Timer) {
+    return (
+      <div>
+        <Timer />
+      </div>
+    );
+  } else if (selectedIcon === Icon.Canvas) {
+    return (
+      <div>
+        <Canvas />
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+};
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,93 @@
-export const Timer = () => {
-    return (<div></div>)
-}
+import { useState } from "react";
+import { AiOutlinePlaySquare, AiOutlinePauseCircle } from "react-icons/ai";
+import { CgShapeSquare } from "react-icons/cg";
 
-export {}
+const buttonStyle: React.CSSProperties = {
+  position: "relative",
+  top: "80px",
+  margin: "20px", // Why more than 20px messes up the button position?
+};
+
+export const Timer = () => {
+  const [time, setTime] = useState(0);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timer | null>(null);
+
+  const playClick = () => {
+    setIntervalId(
+      setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000)
+    );
+  };
+
+  const pauseClick = () => {
+    clearInterval(intervalId as unknown as number); //parameter type of clearInterval and return type of setInterval is not same.
+    setIntervalId(null);
+  };
+
+  const stopClick = () => {
+    clearInterval(intervalId as unknown as number);
+    setIntervalId(null);
+    setTime(0);
+  };
+
+  const timeToMinute = (time: number) => {
+    if (time / 60 < 10) {
+      return `0${Math.floor(time / 60)}`;
+    } else {
+      return `${Math.floor(time / 60)}`;
+    }
+  };
+
+  const timeToSecond = (time: number) => {
+    if (time % 60 < 10) {
+      return `0${time % 60}`;
+    } else {
+      return `${time % 60}`;
+    }
+  };
+
+  return (
+    <div
+      style={{
+        border: "2px solid",
+        width: "600px",
+        height: "400px",
+        margin: "auto",
+      }}
+    >
+      <div
+        style={{
+          border: "4px solid lightblue",
+          width: "400px",
+          height: "140px",
+          margin: "auto",
+          fontSize: "80px",
+          position: "relative",
+          top: "10px",
+        }}
+      >
+        <div
+          style={{
+            position: "relative",
+            top: "10px",
+            margin: "auto",
+          }}
+        >
+          {timeToMinute(time)} : {timeToSecond(time)}
+        </div>
+        <span style={buttonStyle} onClick={playClick}>
+          <AiOutlinePlaySquare size="5rem" />
+        </span>
+        <span style={buttonStyle} onClick={pauseClick}>
+          <AiOutlinePauseCircle size="5rem" />
+        </span>
+        <span style={buttonStyle} onClick={stopClick}>
+          <CgShapeSquare size="5rem" />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export {};
